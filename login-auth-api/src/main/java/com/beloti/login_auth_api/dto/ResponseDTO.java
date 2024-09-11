e73b2e7e46657789d353b58e8818b4a8bf3c088b
Brunno Beloti <belotibrunnomc@hotmail.com>
2024-09-11 10:58:27 -0300

    Subindo para o github

---
+++ login-auth-api/src/main/java/com/beloti/login_auth_api/dto/ResponseDTO.java
@@ -0,0 +1,4 @@
+package com.beloti.login_auth_api.dto;
+
+public record ResponseDTO(String name, String token) {
+}
---
+++ login-auth-api/src/main/java/com/beloti/login_auth_api/repositories/UserRepository.java
@@ -0,0 +1,11 @@
+package com.beloti.login_auth_api.repositories;
+
+import com.beloti.login_auth_api.domain.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface UserRepository extends JpaRepository<User, UUID> {
+    Optional<User> findByEmail(String email);
+}
